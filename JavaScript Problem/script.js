d92564c61d960d170463108aaee9ffc8b83253b9
Pramod-kumar70 <pramod707011@gmail.com>
2024-11-12 15:20:15 +0530

    in progress

--- JavaScript Problem/script.js
+++ JavaScript Problem/script.js
@@ -7,60 +7,60 @@
 // // others get a 5% discount. Return the final price after discount. Include an error check for negative prices or invalid customer types.
 
 
-const calculateDiscount = function(price , customerType){
+// const calculateDiscount = function(price , customerType){
 
-    if(price>0){
+//     if(price>0){
 
-    if(customerType=="Premium")
-    {
-        let FinalDiscounts = price-(price*20)/100;
-        return FinalDiscounts;
-    }
+//     if(customerType=="Premium")
+//     {
+//         let FinalDiscounts = price-(price*20)/100;
+//         return FinalDiscounts;
+//     }
 
-    else if(customerType == "Regular")
-        {
-            let FinalDiscounts = price-(price*10)/100;
-            return FinalDiscounts;
-        }
+//     else if(customerType == "Regular")
+//         {
+//             let FinalDiscounts = price-(price*10)/100;
+//             return FinalDiscounts;
+//         }
     
-    else 
-        {
-            let FinalDiscounts = price-(price*5)/100;
-            return FinalDiscounts;
-        }
+//     else 
+//         {
+//             let FinalDiscounts = price-(price*5)/100;
+//             return FinalDiscounts;
+//         }
     
     
-    }
+//     }
   
-}
+// }
 
-let amount = 10 , customerType = "Regular";
+// let amount = 10 , customerType = "Regular";
 
-let FinalAmount =  calculateDiscount(amount , customerType);
+// let FinalAmount =  calculateDiscount(amount , customerType);
 
-document.write("Customer type = ",customerType); 
-document.write( " <br>Total purchage amount = ",amount); 
-document.write(" <br> Final amount  = ",FinalAmount);
+// document.write("Customer type = ",customerType); 
+// document.write( " <br>Total purchage amount = ",amount); 
+// document.write(" <br> Final amount  = ",FinalAmount);
 
 
-// Question 2 **
+// // Question 2 **
 
-var Arr = [];
+// var Arr = [];
 
-const checkprime = function(num){
-let flag =0
-    for(let i=2;i<num;i++)
-    {
-        if(num%i==0)
-        flag = 1;
-    }
-  if(flag == 0)
-    Arr.push(num)
-}  
+// const checkprime = function(num){
+// let flag =0
+//     for(let i=2;i<num;i++)
+//     {
+//         if(num%i==0)
+//         flag = 1;
+//     }
+//   if(flag == 0)
+//     Arr.push(num)
+// }  
 
-let limit = 30;
-for(let j=2;j<=limit;j++)
-checkprime(j);
+// let limit = 30;
+// for(let j=2;j<=limit;j++)
+// checkprime(j);
 
 
 
@@ -129,34 +129,62 @@ checkprime(j);
 // Deducts the amount from the balance if it's less than or equal to the balance.
 // Adds a 10% penalty if the account type is "current" and the balance becomes negative. Return the updated balance after the operation.
 
-const BankAccount1 = {
-    Balance : 500,
-    AccountType:"Saving",
-   Withdraw:function(Amount){
+// const BankAccount1 = {
+//     Balance : 500,
+//     AccountType:"Saving",
+//    Withdraw:function(Amount){
     
-    this.Balance = this.Balance-Amount;
-   }
-}
+//     this.Balance = this.Balance-Amount;
+//    }
+// }
 
-const BankAccount2 = {
-    Balance:1000,
-    AccountType : "current",
-    Withdraw :function(Amount){
-        if(this.Balance>0)
-        this.Balance= this.Balance-Amount;
-        else{
-            console.log(" Your Balance is less than 0 so 10% penalty got applayed ");
+// const BankAccount2 = {
+//     Balance:1000,
+//     AccountType : "current",
+//     Withdraw :function(Amount){
+//         if(this.Balance>0)
+//         this.Balance= this.Balance-Amount;
+//         else{
+//             console.log(" Your Balance is less than 0 so 10% penalty got applayed ");
             
-            this.Balance = this.Balance - (this.Balance*10)/100;
-        }
-    }
+//             this.Balance = this.Balance - (this.Balance*10)/100;
+//         }
+//     }
+// }
+
+
+// BankAccount1.Withdraw(200);
+// console.log(BankAccount1);
+
+// BankAccount2.Withdraw(900);
+// BankAccount2.Withdraw(500);
+// BankAccount1.Withdraw(200);
+// console.log(BankAccount2);
+
+
+// 6. Student Grades with for loop and switch case
+// Define an array of objects, students, where each student has properties name and score. Write a function assignGrades(students) that assigns each student a grade (A-F) based on their score using a for loop and switch case. Update each student object with a new grade property.
+
+
+const GetGrade = function(MarkList){
+    let Total=0;
+    for(let i=0;i<MarkList.length;i++)      
+       Total = Total + MarkList[i];
+       
+    return Total/MarkList.length;    
+    
 }
 
+const Student1 = {} , Student2 = {} , Student3 = {};
 
-BankAccount1.Withdraw(200);
-console.log(BankAccount1);
+Student1.Name = "Pramod Kumar";
+Student1.Score = [90,78,67,99,66,82];
+Student1.Grade = GetGrade(Student1.Score);
 
-BankAccount2.Withdraw(900);
-BankAccount2.Withdraw(500);
-BankAccount1.Withdraw(200);
-console.log(BankAccount2);
+Student2.Name = "Raja Kumar";
+Student2.Score = [74,87,55,98,88,90];
+// Student2.Grade = GetGrade(Student2.Score,Student2.Score.length);
+
+Student3.Name = "Danny Danger";
+Student3.Score = [69,82,90,60,79,99];
+// Student3.Grade = GetGrade(Student3.Score,Student3.Score.length);
